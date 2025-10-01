577a8872c9626bfff3b9bf3406a7eca8275389fc
Jonathan Gao <gsmlg.com@gmail.com>
2025-10-01 23:55:42 +0800

    fix: Resolve PhoenixStorybook compilation warning by adding content entries to index files

---
+++ apps/duskmoon_storybook_web/lib/storybook/index.exs
@@ -0,0 +1,10 @@
+# This file is required for PhoenixStorybook to work
+# It will be populated with story entries as you add them
+
+[
+  %{
+    kind: :folder,
+    name: "Components",
+    path: "/components"
+  }
+]
