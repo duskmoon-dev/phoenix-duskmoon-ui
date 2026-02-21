76a72e56683f08b719228e9baeb27acc3c5a0f31
Jonathan Gao <gsmlg.com@gmail.com>
2026-02-21 22:43:19 +0800

    test: replace placeholder test with generate_id/0 tests in main test file

--- apps/phoenix_duskmoon/test/phoenix_duskmoon_test.exs
+++ apps/phoenix_duskmoon/test/phoenix_duskmoon_test.exs
@@ -1,11 +1,24 @@
 defmodule PhoenixDuskmoonTest do
   use ExUnit.Case, async: true
 
-  # use PhoenixDuskmoon
-  # import PhoenixDuskmoon
   doctest PhoenixDuskmoon
 
-  test "link_attributes" do
-    assert "a" == "a"
+  describe "PhoenixDuskmoon.Component.generate_id/0" do
+    test "returns a string starting with random- prefix" do
+      id = PhoenixDuskmoon.Component.generate_id()
+      assert String.starts_with?(id, "random-")
+    end
+
+    test "returns a string with 16 hex characters after prefix" do
+      id = PhoenixDuskmoon.Component.generate_id()
+      hex_part = String.replace_prefix(id, "random-", "")
+      assert String.length(hex_part) == 16
+      assert hex_part =~ ~r/^[0-9A-F]{16}$/
+    end
+
+    test "generates unique ids on successive calls" do
+      ids = for _ <- 1..10, do: PhoenixDuskmoon.Component.generate_id()
+      assert length(Enum.uniq(ids)) == 10
+    end
   end
 end
