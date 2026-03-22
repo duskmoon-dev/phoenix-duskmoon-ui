9640050273c41c97aee103684fe15f2bda379fba
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-22 18:08:45 +0800

    chore: bump version to 9.0.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PhoenixDuskmoon.Umbrella.MixProject do
   use Mix.Project
 
   # Umbrella version tracks phoenix_duskmoon package version
-  @version "7.3.0"
+  @version "9.0.0"
 
   def project do
     [
